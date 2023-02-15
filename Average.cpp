6bf423ea22e91e04c48caa660ef5e537bf4af331
SHUlinlulu <linlulu@shu.edu.cn>
2023-02-15 19:10:38 +0800

    New a cpp file to acquire the average of five integer number

---
+++ Average.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5]={12,4,36,45,7};
+    double ave=0.0;
+    int sum=0;
+    for(int i=0;i<5;i++){
+        sum+=arr[i];
+    }
+    ave=(double)sum/5;//强制类型转换
+    cout <<"The average of 5 integer number is "<<ave<<"\n";
+    return 0;
+}
