f5282f1f2a2b4c3eb4776d578c6ccc0ca55357ae
SHUlinlulu <linlulu@shu.edu.cn>
2023-02-15 20:13:27 +0800

    New an new cpp file to check the function of string

---
+++ Test_string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(int argc,char *argv[]){
+    string username="";
+    long password=0;
+    cout <<"Please input the username & password"<<endl;
+    cin >>username>>password;
+    cout <<"The username is "<<username<<" & password is "<<password<<endl;
+    system("pause");
+    return 0;
+}
